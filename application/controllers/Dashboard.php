8bb90a9803942681aa1018dbbcf481d2865e03ce
HasroHafiz <hafiz@moha.gov.my>
2018-08-06 16:38:00 +0800

    add __construct untuk checking if login utk access pages

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,8 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 
 	
-	public function index()
+	public function __construct()
 	{
+		parent::__construct();
+
+		//check if user is logged in
+		if (!$this->ion_auth->logged_in())
+		{
+			redirect('auth/login');
+		}
+	}
+
+
+	public function index()
+	{	
 
 		//declare empty array
 		$data = array();
